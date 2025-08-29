e3d0294ed5b5e608795a218cef6adff184dcf65b
Burhanuddin Salim <burhan.salman@gmail.com>
2025-08-29 07:46:49 +0400

    feat: add other warehouses

--- server/config/warehouse-emails.ts
+++ server/config/warehouse-emails.ts
@@ -1,3 +1,7 @@
 export default {
+  FKWH: "whassistant@sterling.ae",
   API: "b.isaac@sterling.ae",
+  SPI: "akbar.hussain@sterling.ae",
+  DIP1: "shaquid.adil@sterling.ae",
+  RAS: "arka.gayen@sterling.ae",
 };
